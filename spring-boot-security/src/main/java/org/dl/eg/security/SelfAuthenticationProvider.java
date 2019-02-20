1d55a010d2f9a488ed4b5c033e33ff4a8d6cb47b
meiliangdeng <meiliangdeng@163.com>
2019-02-20 21:10:19 +0800

    auth2.0授权配置

--- spring-boot-security/src/main/java/org/dl/eg/security/SelfAuthenticationProvider.java
+++ spring-boot-security/src/main/java/org/dl/eg/security/SelfAuthenticationProvider.java
@@ -1,7 +1,25 @@
 package org.dl.eg.security;
 
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by yituiyun on 2019/1/27.
  */
-public class SelfAuthenticationProvider {
+@Component
+public class SelfAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String userName = (String) authentication.getPrincipal(); // 这个获取表单输入中返回的用户名;
+        String password = (String) authentication.getCredentials(); // 这个是表单中输入的密码；
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return true;
+    }
 }
