b87f1dce25225ff89a7b464c348c519b9cad7b00
meiliangdeng <meiliangdeng@163.com>
2019-03-02 20:09:50 +0800

    授权服务器和资源服务器

---
+++ spring-boot-oauth2-resource/src/main/java/org/dl/config/MyResourceServerConfig.java
@@ -0,0 +1,11 @@
+package org.dl.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class MyResourceServerConfig extends ResourceServerConfigurerAdapter {
+}
---
+++ spring-boot-oauth2/src/main/java/org/dl/eg/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package org.dl.eg.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)  //  启用方法级别的权限认证
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+}
